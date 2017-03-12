15bb2d7806012c8bf6aebd6b13dac08f8058d6d6
xzj <xzj007@gmail.com>
2017-03-12 12:49:05 +0800

    Removed newline
    	modified:   project.clj

--- project.clj
+++ project.clj
@@ -2,7 +2,6 @@
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :profiles {:dev {:dependencies [[midje "1.8.3"]]
-                   :plugins [
-                             [lein-cloverage "1.0.9"]
+                   :plugins [[lein-cloverage "1.0.9"]
                              ]}}
   )
